459a4956ff38cdacd95eaa4e36568d1973474c62
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-08-21 11:47:17 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    int[] parent;
+    int count ;
+    public int countComponents(int n, int[][] edges) {
+        count = n;
+        parent = new int[n];
+
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        for(int[] edge:edges ){
+            union(edge[0],edge[1]);
+        }
+        return count;
+    }
+    
+    private void union(int a,int b){
+        int rootA = find(a);
+        int rootB = find(b);
+       
+        if(rootA!=rootB){
+            count--;
+        }
+        parent[rootB] = rootA;
+
+    }
+
+    private int find(int x){
+        if(parent[x]==x){
+            return x;
+        }
+        return find(parent[x]);
+    }
+}
